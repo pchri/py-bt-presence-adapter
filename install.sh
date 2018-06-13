c229f57b3946fde7a93562f22704a313aaf77fa4
Poul Christiansen <pchris@hitmand.d>
2018-06-13 19:02:53 +0000

    Prepare version 0.1.0

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo apt-get install libcap2-bin python3-pip libbluetooth-dev libglib2.0-dev libboost-thread-dev libboost-python-dev
-sudo python3 -m pip install pybluez pygattlib
-# sudo setcap cap_net_raw,cap_net_admin+eip `readlink -f \`which python3\``
